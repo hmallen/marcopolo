2ddefcb236e2cedb226da9b429a8fe810b58b256
hmallen <allenhm@gmail.com>
2018-06-16 23:04:34 -0500

    Finished fully-functional version of ticker with Slack output.

--- marcopolo/ticker.py
+++ marcopolo/ticker.py
@@ -1,3 +1,5 @@
+import configparser
+import datetime
 import json
 import logging
 from multiprocessing.dummy import Process as Thread
@@ -5,19 +7,24 @@ import time
 
 from poloniex import Poloniex
 from pymongo import MongoClient
+from slackclient import SlackClient
 import websocket
+
+config_path = '../config/config.ini'
+
+mongo_ip_local = 'mongodb://192.168.1.179:27017/'
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-class wsTicker(object):
+class Ticker(object):
 
-    def __init__(self):
+    def __init__(self, slack_info, mongo_ip):
         self.api = Poloniex()
 
-        self.db = MongoClient('mongodb://192.168.1.179:27017/').poloniex['ticker']
+        self.db = MongoClient(mongo_ip).poloniex['ticker']
 
         self.db.drop()
 
@@ -27,6 +34,13 @@ class wsTicker(object):
                                          on_close=self.on_close)
 
         self.ws.on_open = self.on_open
+
+        self.slack_client = slack_info['client']
+
+        self.slack_channel_id_alerts = slack_info['channels']['alerts'][1]
+        self.slack_channel_id_exceptions = slack_info['channels']['exceptions'][1]
+
+        self.last_update = None
 
 
     def __call__(self, market=None):
@@ -43,6 +57,8 @@ class wsTicker(object):
         if 'error' in message:
             #print(message['error'])
             logger.error(message['error'])
+
+            # SEND SLACK EXCEPTION MESSAGE HERE
 
             return
 
@@ -91,15 +107,30 @@ class wsTicker(object):
                 upsert=True)
             """
 
+            self.last_update = datetime.datetime.now()
+
 
     def on_error(self, ws, error):
         #print(error)
         logger.error(error)
 
+        slack_message = 'Error returned from websocket connection:\n'
+        slack_message += str(error)
+
+        slack_return = Ticker.send_slack_alert(self, channel_id=self.slack_channel_id_alerts, message=slack_message)
+
+        logger.debug('slack_return: ' + str(slack_return))
+
 
     def on_close(self, ws):
         #print("Websocket closed!")
         logger.debug('Websocket closed.')
+
+        slack_message = 'Websocket closed.'
+
+        slack_return = Ticker.send_slack_alert(self, channel_id=self.slack_channel_id_alerts, message=slack_message)
+
+        logger.debug('slack_return: ' + str(slack_return))
 
 
     def on_open(self, ws):
@@ -112,10 +143,24 @@ class wsTicker(object):
                 upsert=True)
 
         #print('Populated markets database with ticker data')
-        logger.debug('Populated markets database with ticker data.')
+        logger.debug('Populated markets database with ticker data from REST API.')
+
+        slack_message = 'MongoDB populated with REST API market ticker data.'
+
+        slack_return = Ticker.send_slack_alert(self, channel_id=self.slack_channel_id_alerts, message=slack_message)
+
+        logger.debug('slack_return: ' + str(slack_return))
+
+        #self.last_update = datetime.datetime.now()
 
         self.ws.send(json.dumps({'command': 'subscribe',
                                  'channel': 1002}))
+
+        slack_message = 'Subscribed to ticker websocket.'
+
+        slack_return = Ticker.send_slack_alert(self, channel_id=self.slack_channel_id_alerts, message=slack_message)
+
+        logger.debug('slack_return: ' + str(slack_return))
 
 
     def start(self):
@@ -128,6 +173,12 @@ class wsTicker(object):
         #print('Thread started')
         logger.debug('Thread started.')
 
+        slack_message = 'Ticker startup initialized.'
+
+        slack_return = Ticker.send_slack_alert(self, channel_id=self.slack_channel_id_alerts, message=slack_message)
+
+        logger.debug('slack_return: ' + str(slack_return))
+
 
     def stop(self):
         self.ws.close()
@@ -137,18 +188,175 @@ class wsTicker(object):
         #print('Thread joined')
         logger.debug('Thread joined.')
 
+        slack_message = 'Ticker shutdown complete.'
+
+        slack_return = Ticker.send_slack_alert(self, channel_id=self.slack_channel_id_alerts, message=slack_message)
+
+        logger.debug('slack_return: ' + str(slack_return))
+
+
+    def send_slack_alert(self, channel_id, message):
+        alert_return = {'Exception': False, 'result':{}}
+
+        try:
+            alert_return['result'] = slack_client.api_call(
+                'chat.postMessage',
+                channel=channel_id,
+                text=message,
+                username=slack_bot_user,
+                icon_url=slack_bot_icon
+            )
+
+        except Exception as e:
+            logger.exception('Exception raised in Ticker.send_slack_alert().')
+            logger.exception(e)
+
+            alert_return['Exception'] = True
+
+        finally:
+            return alert_return
+
 
 if __name__ == "__main__":
     try:
+        config = configparser.ConfigParser()
+        config.read(config_path)
+
+        slack_token = config['slack']['token']
+        slack_channel_alerts = config['slack']['channel_alerts']
+        slack_channel_exceptions = config['slack']['channel_exceptions']
+        slack_bot_user = config['slack']['bot_user']
+        slack_bot_icon = config['slack']['bot_icon']
+
+        logger.info('Initializing Slack client.')
+
+        # Slack connection
+        slack_client = SlackClient(slack_token)
+
+        channel_list = slack_client.api_call('channels.list')
+        group_list = slack_client.api_call('groups.list')
+
+        slack_channel_targets = {'alerts': slack_channel_alerts,
+                                 'exceptions': slack_channel_exceptions}
+
+        for target in slack_channel_targets:
+            try:
+                logger.debug('channel_list.get(\'ok\'): ' + str(channel_list.get('ok')))
+                if channel_list.get('ok'):
+                    for chan in channel_list['channels']:
+                        logger.debug('chan[\'name\']: ' + chan['name'])
+                        if chan['name'] == slack_channel_targets[target]:
+                            if target == 'alerts':
+                                slack_channel_id_alerts = chan['id']
+
+                            elif target == 'exceptions':
+                                slack_channel_id_exceptions = chan['id']
+
+                            break
+                    else:
+                        logger.error('No valid Slack channel found for alert in channel list.')
+
+                        sys.exit(1)
+
+                else:
+                    logger.error('Channel list API call failed.')
+
+                    sys.exit(1)
+
+            except:
+                logger.debug('group_list.get(\'ok\'): ' + str(group_list.get('ok')))
+                if group_list.get('ok'):
+                    for group in group_list['groups']:
+                        logger.debug('group[\'name\']: ' + group['name'])
+                        if group['name'] == slack_channel_targets[target]:
+                            if target == 'alerts':
+                                slack_channel_id_alerts = group['id']
+
+                            elif target == 'exceptions':
+                                slack_channel_id_exceptions = group['id']
+
+                            break
+                    else:
+                        logger.error('No valid Slack channel found for alert in group list.')
+
+                        sys.exit(1)
+
+                else:
+                    logger.error('Group list API call failed.')
+
+                    sys.exit(1)
+
+        logger.info('Slack channel for alerts: #' + slack_channel_alerts +
+                    ' (' + slack_channel_id_alerts + ')')
+
+        logger.info('Slack channel for exceptions: #' + slack_channel_exceptions +
+                    ' (' + slack_channel_id_exceptions + ')')
+
+        slack_info = dict(client=slack_client,
+                          token=slack_token,
+                          channels=dict(alerts=(slack_channel_alerts, slack_channel_id_alerts),
+                                        exceptions=(slack_channel_exceptions, slack_channel_id_exceptions)),
+                          bot=dict(user=slack_bot_user,
+                                   icon=slack_bot_icon))
+
+        logger.info('Initializing ticker.')
+
         # websocket.enableTrace(True)
 
-        ticker = wsTicker()
+        ticker = Ticker(slack_info=slack_info, mongo_ip=mongo_ip_local)
+
+        logger.info('Starting ticker thread.')
 
         ticker.start()
 
+        logger.info('Waiting for ticker to be ready.')
+
+        while ticker.last_update == None:
+            logger.debug('ticker.last_update: ' + str(ticker.last_update))
+
+            time.sleep(1)
+
+        logger.debug('ticker.last_update: ' + str(ticker.last_update))
+
+        last_update = ticker.last_update
+
+        error_timeout = datetime.timedelta(seconds=30)
+
+        error_message_sent = False
+
+        error_message_time = None
+
+        error_message_reset = datetime.timedelta(minutes=10)
+
+        slack_message = 'Real-time Poloniex ticker data ready for use at ' + mongo_ip_local + '.'
+
+        slack_return = ticker.send_slack_alert(channel_id=slack_channel_id_alerts, message=slack_message)
+
+        logger.debug('slack_return: ' + str(slack_return))
+
+        logger.info('Beginning monitoring loop.')
+
         while (True):
             try:
-                time.sleep(0.1)
+                #logger.debug('ticker.last_update: ' + str(ticker.last_update))
+                if (datetime.datetime.now() - ticker.last_update) > error_timeout:
+                    if error_message_sent == False:
+                        error_message = '*NO TICKER DATA RECEIVED IN 30 SECONDS. AN ERROR HAS OCCURRED THAT REQUIRES IMMEDIATE ATTENTION.*'
+
+                        slack_return = ticker.send_slack_alert(channel_id=slack_channel_id_alerts, message=error_message)
+
+                        logger.debug('slack_return: ' + str(slack_return))
+
+                        error_message_sent = True
+
+                        error_message_time = datetime.datetime.now()
+
+                if error_message_sent == True and (datetime.datetime.now() - error_message_time) > error_message_reset:
+                    logger.info('Resetting error message sent switch to allow another alert.')
+
+                    error_message_sent = False
+
+                time.sleep(1)
 
             except Exception as e:
                 logger.exception('Exception in inner loop.')
